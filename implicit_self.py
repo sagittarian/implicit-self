54d5a9739f3052160d57e4a9725dd0a12b9ec1f4
Adam Mesha <adam@mesha.org>
2014-02-11 00:50:19 +0200

    Another way to do this using the dynamic call stack

--- implicit_self.py
+++ implicit_self.py
@@ -53,6 +53,8 @@ class ImplicitSelfMetaclass(type):
 
 
 class ImplicitSelf(metaclass=ImplicitSelfMetaclass):
+    '''Inherit from this class to have implicit self and cls defined
+    within your method bodies, using global namespace munging.'''
     pass
 
 
@@ -79,3 +81,69 @@ class A(ImplicitSelf):
         print('self is {}'.format(self))
         print('cls is {}'.format(cls))
 
+
+
+### Second approach: using sys._getframe ###
+
+import sys
+
+def resolve_dynamic_var(name):
+    frame = sys._getframe(0)
+    while frame is not None:
+        if name in frame.f_locals:
+            return frame.f_locals[name]
+        frame = frame.f_back
+
+def self():
+    return resolve_dynamic_var('self')
+
+def cls():
+    return resolve_dynamic_var('cls')
+
+
+class SelfClsDescriptor:
+
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, obj, cls_=None):
+
+        func = self.func
+
+        def decorated(*args, **kw):
+            self = obj
+            cls = type(obj) if obj is not None else cls_
+
+            return func(*args, **kw)
+
+        return decorated
+
+class SelfClsMetaclass(type):
+
+    def __new__(cls, name, bases, d):
+        for key in d:
+            if callable(d[key]):
+                d[key] = SelfClsDescriptor(d[key])
+        return type.__new__(cls, name, bases, d)
+
+class SelfCls(metaclass=SelfClsMetaclass):
+    '''Methods found in classes inheriting from this class can call the
+    global functions self() and cls() to dynamically resolve their own
+    instance object and class, without explicitly declaring them.
+
+    '''
+    pass
+
+class B(SelfCls):
+    '''
+    >>> from implicit_self import B
+    >>> b = B()
+    >>> b.m()
+    self() is <implicit_self.B object at 0x1182cd0>
+    cls() is <class 'implicit_self.B'>
+    >>>
+    '''
+
+    def m():
+        print('self() is {}'.format(self()))
+        print('cls() is {}'.format(cls()))
